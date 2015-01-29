5b5df63745b5689a182b0a39616332870f6ca3a9
Tsahi <t.tsahi@gmail.com>
2015-01-29 10:39:41 +0200

    Add traceur compiler to gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var ngAnnotate = require('gulp-ng-annotate');
+var traceur = require('gulp-traceur');
 var uglify = require('gulp-uglify');
 
+gulp.task('compile', function(){
+  return gulp.src('src/**/*.js')
+    .pipe(traceur({experimental: true, blockBinding: true, arrayComprehension: true}))
+    .pipe(gulp.dest('build'));
+});
+
 gulp.task('build', function() {
-  gulp.src('src/**/*.js')
+  return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
+    .pipe(traceur({experimental: true, blockBinding: true, arrayComprehension: true}))
     .pipe(concat('angular-cesium.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'));
@@ -14,10 +22,13 @@ gulp.task('build', function() {
 
 gulp.task('dist', function () {
   return gulp.src('src/**/*.js')
+    .pipe(traceur({experimental: true, blockBinding: true, arrayComprehension: true}))
     .pipe(concat('angular-cesium.min.js'))
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['build', 'dist']);
+gulp.task('default', ['compile', 'build', 'dist']);
+
+gulp.watch('src/**/*.js', ['default']);
