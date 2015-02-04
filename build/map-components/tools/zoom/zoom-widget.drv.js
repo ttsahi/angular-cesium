446ab54f3329d515f5310e1bd42a3a1313910c9d
Tsahi <t.tsahi@gmail.com>
2015-02-04 11:14:56 +0200

    add karma conf file.

---
+++ build/map-components/tools/zoom/zoom-widget.drv.js
@@ -0,0 +1,101 @@
+"use strict";
+(function(angular) {
+  'use strict';
+  angular.module('angularCesium').directive('zoomWidget', ['$document', function($document) {
+    return {
+      replace: true,
+      require: '^tool',
+      template: '<div class="zoom-widget">' + '<div class="zoom-in-btn">' + '<button type="button" class="btn btn-default" ng-click="zoomIn();">' + '<span class="glyphicon glyphicon-zoom-in" aria-hidden="true"></span>' + '</button>' + '</div>' + '<div class="slider">' + '<span class="bar">' + '</span>' + '<span class="pointer">' + '</span>' + '</div>' + '<div class="zoom-out-btn">' + '<button type="button" class="btn btn-default" ng-click="zoomOut();">' + '<span class="glyphicon glyphicon-zoom-out" aria-hidden="true"></span>' + '</button>' + '</div>' + '</div>',
+      link: function(scope, element, attrs, toolCtrl) {
+        if (isFinite(attrs.width) || isFinite(attrs.height)) {
+          var width = isFinite(attrs.width) ? Number.parseInt(attrs.width) + 'px' : 'inherit';
+          var height = isFinite(attrs.height) ? Number.parseInt(attrs.height) + 'px' : 'inherit';
+          element.css({
+            position: 'relative',
+            width: width,
+            height: height
+          });
+        }
+        var minLevel = isFinite(attrs.min) ? Number.parseInt(attrs.min) : 0;
+        var maxLevel = isFinite(attrs.max) ? Number.parseInt(attrs.max) : 100;
+        if (minLevel < 0 || maxLevel < 0 || minLevel >= maxLevel) {
+          throw new Error("min or max attributes value are invalid.");
+        }
+        var jumps = isFinite(attrs.jump) ? Number.parseInt(attrs.jump) : 10;
+        var zoomTool = toolCtrl.getTool();
+        var levelValue = 90 / (maxLevel - minLevel);
+        var currentLevel = (maxLevel - minLevel) / 2;
+        var zoomLevel = (maxLevel + minLevel) / 2;
+        var tempLevel = zoomLevel;
+        var currentPointerHeight = 45;
+        var pointer = angular.element(element.find('span')[2]);
+        pointer.css('top', currentLevel * levelValue + '%');
+        var clientY = null;
+        var barHeight = pointer[0].clientHeight * 10;
+        var startPointerHeight = currentPointerHeight;
+        pointer.on('mousedown', (function(event) {
+          event.preventDefault();
+          clientY = event.clientY;
+          startPointerHeight = currentPointerHeight;
+          tempLevel = zoomLevel;
+          pointer.addClass('active');
+        }));
+        $document.on('mouseup', (function(event) {
+          if (clientY !== null) {
+            clientY = null;
+            pointer.removeClass('active');
+            console.log(zoomLevel);
+          }
+        }));
+        $document.on('mousemove', (function(event) {
+          if (clientY !== null) {
+            var deltaY = clientY - event.clientY;
+            var percent = (Math.abs(deltaY) * 100 / barHeight);
+            if (deltaY > 0) {
+              if (startPointerHeight - percent >= 0) {
+                currentPointerHeight = startPointerHeight - percent;
+              } else {
+                currentPointerHeight = 0;
+              }
+            } else {
+              if (startPointerHeight + percent <= 90) {
+                currentPointerHeight = startPointerHeight + percent;
+              } else {
+                currentPointerHeight = 90;
+              }
+            }
+            currentLevel = Math.trunc(currentPointerHeight / levelValue);
+            zoomLevel = maxLevel - currentLevel;
+            if (zoomLevel > tempLevel) {
+              zoomTool.zoomIn((zoomLevel - tempLevel) * jumps);
+            }
+            if (zoomLevel < tempLevel) {
+              zoomTool.zoomOut((tempLevel - zoomLevel) * jumps);
+            }
+            tempLevel = zoomLevel;
+            pointer.css('top', currentLevel * levelValue + '%');
+          }
+        }));
+        scope.zoomIn = function() {
+          if (zoomLevel < maxLevel) {
+            zoomLevel++;
+            currentLevel--;
+            currentPointerHeight = currentLevel * levelValue;
+            pointer.css('top', currentPointerHeight + '%');
+            zoomTool.zoomIn(jumps);
+          }
+        };
+        scope.zoomOut = function() {
+          if (zoomLevel > minLevel) {
+            zoomLevel--;
+            currentLevel++;
+            currentPointerHeight = currentLevel * levelValue;
+            pointer.css('top', currentPointerHeight + '%');
+            zoomTool.zoomOut(jumps);
+          }
+        };
+      }
+    };
+  }]);
+}(window.angular));
+//# sourceURL=map-components/tools/zoom/zoom-widget.drv.js
