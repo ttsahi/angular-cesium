888cd6f40c9759e0fc1a10414d6cc9670285ffcf
Erez <erezyyy@gmail.com>
2015-02-04 11:56:39 +0200

    Merge branch 'master' of https://github.com/ttsahi/angular-cesium
    Conflicts:
    	dist/angular-cesium.js
    	dist/angular-cesium.min.js

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Cherome'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
