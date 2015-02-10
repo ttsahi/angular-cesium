017ee37593303a24462a129e3c0ccfbeb66627ee
Tsahi <t.tsahi@gmail.com>
2015-02-10 17:27:04 +0200

    Remove link from toolbar

--- src/map-components/toolbar/toolbar.js
+++ src/map-components/toolbar/toolbar.js
@@ -9,11 +9,9 @@ angular.module('angularCesium').directive('toolbar', function() {
     restrict : 'E',
     template : '<div class="toolbar" ><div class="drag-button glyphicon glyphicon-minus" draggable></div> <ng-transclude></ng-transclude></div>',
     transclude : true,
-    require : '^map',
-    controller : function($scope) {
+    require : '^^map',
+    controller : function() {
       let currentTool = null;
-
-      this.getCesiumWidget = () => $scope.getCesiumWidget();
 
       this.startTool = function(tool){
         if(currentTool !== null){
@@ -23,13 +21,6 @@ angular.module('angularCesium').directive('toolbar', function() {
         currentTool = tool;
         currentTool.start();
       };
-    },
-    link : {
-      pre: function(scope, element, attrs, mapCtrl){
-        scope.getCesiumWidget = mapCtrl.getCesiumWidget;
-        scope.mapRect = function(){ return angular.element(scope.getCesiumWidget().canvas)[0].getBoundingClientRect();
-        };
-      }
     }
   };
 });
